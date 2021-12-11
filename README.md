d7199e5c6054b3479fdaf14be42d2b40da01338e
David Groomes <dwgroomes@gmail.com>
2021-12-11 12:25:50 -0600

    Upgrade to Gradle 7.3.1; Java 17; and upgrade libraries

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # picocli-playground
 
-NOT YET IMPLEMENTED
+NOT YET FULLY IMPLEMENTED
 
-📚 Learning and exploring Picocli <https://picocli.info/>
+📚 Learning and exploring picocli.
 
-## Gradle + Java 15
-
-This project uses Java 15 and the new `Records` language feature <https://openjdk.java.net/jeps/359>.
-
-Unfortunately, I can't figure out how to configure Gradle to successfully execute the "test" task using Java 15. Instead,
-I am relying on the Standalone JUnit Console Launcher to work around this limitation. Read more at [Instructions](#instructions).  
+> picocli - a mighty tiny command line interface
+>
+> -- <cite>https://picocli.info/</cite>
 
 ## Instructions
 
-1. Set the environment variable `JAVA_15_HOME` to the path of a JDK 15 installation on your computer
-1. Build the program and run the tests with `./test.sh`
-    * This uses the Standalone JUnit Console Launcher <https://junit.org/junit5/docs/current/user-guide/#running-tests-console-launcher>
-      and Java 15
-1. Build and run the program with `./gradlew run`
+Follow the below instructions to build and run the program.
+
+1. Use Java 17
+1. Build the program and run the tests:
+   * `./gradlew test`
+1. Build and run the program: 
+   * `./gradlew run`
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,98 +1,39 @@
-/**
- * This Gradle build script uses the Kotlin form.
- *
- * The official documentation goes a long way in explaining the differences between a traditional Groovy Gradle build
- * script (build.gradle) and the Kotlin form (build.gradle.kts) https://docs.gradle.org/current/userguide/kotlin_dsl.html
- */
-
 plugins {
     java
     application
 }
 
-val slf4jVersion = "1.7.26"
-val junitPlatformVersion = "1.6.0"
-val junitJupiterVersion = "5.6.0"
-val picocliVersion = "4.2.0"
-val jacksonVersion = "2.10.1"
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+val slf4jVersion = "1.7.32" // SLF4J releases: http://www.slf4j.org/news.html
+val junitVersion = "5.8.2" // JUnit releases: https://github.com/junit-team/junit5/releases
+val assertJVersion = "3.21.0" // AssertJ releases: https://github.com/assertj/assertj-core/tags
+val picocliVersion = "4.6.2" // picocli releases: https://github.com/remkop/picocli/releases
 
 repositories {
     mavenCentral()
 }
 
-configurations.register("junitLauncher")
-
 dependencies {
-    "junitLauncher"("org.junit.platform:junit-platform-console-standalone:$junitPlatformVersion")
-
     implementation("info.picocli:picocli:$picocliVersion")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("org.slf4j:slf4j-simple:$slf4jVersion")
 
-    // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-
-    // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:$assertJVersion")
 }
 
 application {
-    // Define the main class for the application.
-    mainClassName = "dgroomes.Main"
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_15
+    mainClass.set("dgroomes.Main")
 }
 
 tasks {
-    /**
-     * Configure the compiler step to accommodate Java preview features
-     */
-    withType(JavaCompile::class.java) {
-        options.compilerArgs.addAll(arrayOf("--enable-preview"))
-    }
-
-    named<CreateStartScripts>("startScripts") {
-        defaultJvmOpts = listOf("--enable-preview")
-    }
-
-    named<JavaExec>("run") {
-        jvmArgs = listOf("--enable-preview")
-    }
-}
-
-/**
- * Print the path to the standalone JUnit Console Launcher JAR so it can be used later by "test.sh" to execute the JUnit
- * tests.
- *
- * Editorial: It's great to leverage Gradle for managing dependencies and setting up environment information for a
- * downstream (non-Gradle) process like "test.sh".
- */
-tasks.register("printJunitLauncherPath") {
-    doLast {
-        configurations {
-            "junitLauncher" {
-                // "resolve" the configuration and do a "first()" because the "junitLauncher" configuration has only the
-                // one dependency: "junit-platform-console-standalone.jar"
-                val pathToJunitLauncherJar = resolve().first().toString()
-                val pathFile = File(buildDir, "junit-launcher-path.txt")
-                pathFile.writeText(pathToJunitLauncherJar)
-            }
-        }
-    }
-}
-
-/**
- * Support the standalone JUnit Console Launcher by printing the test class path to a file. This task is used in
- * conjunction with 'printJunitLauncherPath'.
- */
-tasks.register("printTestClassPath") {
-    doLast {
-        val sourceSetContainer = project.the<SourceSetContainer>()
-        val classpathObj = sourceSetContainer["test"].runtimeClasspath
-        val classpath = classpathObj.joinToString(separator = ":")
-        val pathFile = File(buildDir, "test-classpath.txt")
-        pathFile.writeText(classpath)
+    test {
+        useJUnitPlatform()
     }
 }
